f4f3f5eb06c236125ebe23ed3dcf83cc880caa7b
Maciej Moniuszko <moniuszko@gmail.com>
2020-02-09 11:52:17 +0100

    Refactored to add project Lombok.

--- src/main/java/com/marcellus/recipes/controllers/IndexController.java
+++ src/main/java/com/marcellus/recipes/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package com.marcellus.recipes.controllers;
 
 import com.marcellus.recipes.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Optional;
 
+@Slf4j
 @Controller
 @RequestMapping({"","/"})
 public class IndexController {
@@ -21,7 +22,7 @@ public class IndexController {
 
     @RequestMapping({"","/","index","index.html"})
     public String getIndexPage(Model model) {
-
+        log.debug("Inside getIndexPage method in controller");
         model.addAttribute("recipes", recipeService.findAll());
 
         return "index";
