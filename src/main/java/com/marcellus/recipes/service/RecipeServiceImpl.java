f1ef345efb431c4a382ca8b74b8e090dc5c3fdd0
Maciej Moniuszko <moniuszko@gmail.com>
2020-02-14 21:05:33 +0100

    Added show recipe by id using TDD.

--- src/main/java/com/marcellus/recipes/service/RecipeServiceImpl.java
+++ src/main/java/com/marcellus/recipes/service/RecipeServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Slf4j
@@ -30,7 +31,14 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Recipe findById(Long id) {
-        return recipeRepository.findById(id).get();
+
+        Optional<Recipe> optionalRecipe = recipeRepository.findById(id);
+
+        if(!optionalRecipe.isPresent()){
+            throw new RuntimeException("Recipe not found");
+        }
+
+        return optionalRecipe.get();
     }
 
     @Override
